3d69695df4517200dbd3eca73c78b98acea3bd23
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-09-18 22:07:21 +0900

    paiza/python3入門

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 print('Hello World!')
+
+# カンマ区切りで半角空いた出力ができる
+print('Hello', 'paiza')
+
+#　print 関数を使うとデフォルトで末尾に改行が入るが、end= で文字列を指定することで末尾を変更できる
+print('Hello', end=",")
+print('World')
+
+print('Hello', end="")
+print('World')
