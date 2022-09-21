cce5e8bff1238f9d7266e4cefae5ef43fd5caf00
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-09-21 21:51:59 +0900

    if文

--- paiza_nyumon.py
+++ paiza_nyumon.py
@@ -8,3 +8,13 @@ print(str(number) + "匹のこぶた")
 # 指定した範囲でランダムな数値を出力する
 number = random.randint(1,100)
 print(str(number) + "匹のこぶた")
+
+# ランダムなサイコロの値を出力
+x = random.randint(1,6)
+print("サイコロの目は" + str(x) + "です")
+
+# if文
+number = random.randint(1, 3)
+print("あなたの順位は" + str(number) + "位です")
+if number == 1:
+    print("おめでとう")
