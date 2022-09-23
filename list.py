2a07e7b9c61639aaa124991c132a0e0dfdea6fb1
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-09-23 21:11:05 +0900

    リストを使ったランダムなくじ

--- list.py
+++ list.py
@@ -99,3 +99,16 @@ import sys
 for line in sys.stdin.readlines():
     enemy = line.rstrip().split(",")
     print(enemy[0] + "が" + enemy[1] + "匹現れた")
+
+# リストを使ったランダムくじ
+# スライム,モンスター,ドラゴン,魔王
+import random
+line = input().rstrip().split(",")
+for enemy in line:
+    print(enemy + "が現れた！")
+
+num = len(line)
+print("敵は" + str(num) + "匹")
+
+attack = random.randrange(num)
+print(line[attack] + "に会心の一撃！" + line[attack] + "を倒した！")
