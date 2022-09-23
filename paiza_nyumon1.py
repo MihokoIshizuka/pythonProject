ed86b3664510061d6b23ee614d65f6571fb5e5ce
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-09-23 16:07:17 +0900

    ループ処理

--- paiza_nyumon1.py
+++ paiza_nyumon1.py
@@ -12,4 +12,29 @@ for i in range(1, 13):
 i = 0
 while i <= 5:
     print(i)
+    # カウンタ変数の更新が必須！
     i += 1
+
+# スライムを倒すプログラム
+import random
+hp = 30
+while hp > 0:
+    hit = random.randint(1, 10)
+    print("スライムに" +str(hit) + "のダメージを与えた");
+    hp -= hit
+print("スライムを倒した")
+
+# 偶数の出力プログラム
+i = 1
+while i <= 10:
+    if (i % 2 == 0):
+        print(i)
+    i += 1
+
+# HTMLで年齢のセレクトボックス
+print("<select name=\'age\'>")
+# rangeの場合は0~9歳が選択される
+for age in range(10):
+    # +1することでageの範囲が1~10歳にずれる
+    print("<option>" + str(age + 1) + "歳</option>")
+print("</select>")
