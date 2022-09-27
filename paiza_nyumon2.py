49d081f3d44155d888999bf70a58e1755e4dc730
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-09-27 15:33:22 +0900

    アイテムリストの作成

--- paiza_nyumon2.py
+++ paiza_nyumon2.py
@@ -35,3 +35,26 @@ sum = 0
 for (key,point) in points.items():
     sum += point
 print(sum)
+
+# 辞書の整列
+weapons = {"イージスソード":40, "ウィンドスピア":12, "アースブレイカー":99}
+print(sorted(weapons.items()))
+print(sorted(weapons))
+
+# アイテムリストの作成
+# 画像用辞書
+items_imges = {
+    "剣" : "http://paiza.jp/learning/images/sword.png",
+    "盾" : "http://paiza.jp/learning/images/shield.png",
+    "回復薬" : "http://paiza.jp/learning/images/potion.png",
+    "クリスタル" : "http://paiza.jp/learning/images/crystal.png"
+}
+# アイテムを入力する個数を決めて入力
+num = int(input())
+li =[]
+# アイテムの個数分リストの要素を作成
+for i in range(num):
+    li.append(input())
+# リストのアイテムを一つずつ画像に変換
+for item_name in li:
+    print("<img src='" + items_imges[item_name] + "'>")
