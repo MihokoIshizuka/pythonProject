8b4294ef66f91460752b79db9e20ec00ef0a1ff3
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-09-27 12:02:18 +0900

    辞書の基本動作

--- paiza_nyumon2.py
+++ paiza_nyumon2.py
@@ -5,3 +5,20 @@ print(enemyDictionary["ザコ"])
 
 level = "ザコ"
 print(enemyDictionary[level])
+
+# 辞書の基本操作
+enemies = {"ザコ":"スライム", "中ボス":"ドラゴン", "ラスボス":"魔王"}
+print(enemies)
+print(len(enemies))
+
+enemies["ザコ2"] = "メタルモンスター"
+print(enemies)
+print(len(enemies))
+
+enemies["中ボス"] = "レッドドラゴン"
+print(enemies)
+print(len(enemies))
+
+del enemies["ザコ"]
+print(enemies)
+print(len(enemies))
