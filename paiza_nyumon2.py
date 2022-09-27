074887bf24422a11b83cb542852de39c736919a4
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-09-27 14:22:24 +0900

    ループ処理で合計の計算

--- paiza_nyumon2.py
+++ paiza_nyumon2.py
@@ -10,15 +10,28 @@ print(enemyDictionary[level])
 enemies = {"ザコ":"スライム", "中ボス":"ドラゴン", "ラスボス":"魔王"}
 print(enemies)
 print(len(enemies))
-
+# 要素の追加
 enemies["ザコ2"] = "メタルモンスター"
 print(enemies)
 print(len(enemies))
-
+#要素の上書き
 enemies["中ボス"] = "レッドドラゴン"
 print(enemies)
 print(len(enemies))
-
+# 要素の削除
 del enemies["ザコ"]
 print(enemies)
 print(len(enemies))
+
+# 辞書のループ処理
+for rank in enemies :
+    print(rank + "があらわれた！")
+for (rank, enemy) in enemies.items():
+    print(rank + "の" + enemy + "があらわれた！")
+
+# ループ処理で点数の合計を計算
+points = {"国語" : 70, "算数" : 35, "英語" : 52}
+sum = 0
+for (key,point) in points.items():
+    sum += point
+print(sum)
