18ff64f18d972f50cd088b9bd7ea59434735eeda
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-09-27 17:04:27 +0900

    ループでリストの処理

--- paiza_nyumon2.py
+++ paiza_nyumon2.py
@@ -58,3 +58,17 @@ for i in range(num):
 # リストのアイテムを一つずつ画像に変換
 for item_name in li:
     print("<img src='" + items_imges[item_name] + "'>")
+
+# ループでリストを処理する
+team = ["勇者", "戦士", "魔法使い"]
+print(team)
+print(team[0])
+
+for (i, person) in enumerate(team):
+    print(str(i + 1) + "番目の" + person + "が、スライムと戦った")
+
+numbers = [3, 1, 4, 1, 5]
+results = []
+for item in numbers:
+    results.append(item * 10)
+print(results)
