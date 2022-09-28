d2f091a3c2cc35122af14ab5d16bd44c89a8a451
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-09-28 23:05:35 +0900

    enumerate

--- paiza_nyumon2.py
+++ paiza_nyumon2.py
@@ -144,3 +144,17 @@ text = ["吾輩は猫である",
 #ここに、行番号を表示するコードを記述する
 for (i,line) in enumerate(text):
     print(str(i) + ":" + line)
+
+# 2次元リストで地図を表示する
+landmap = [["森" for i in range(20)] for j in range(10)]
+landmap[0][0] = "城"
+landmap[0][19] = "町"
+landmap[9][19] = "町"
+for i,line in enumerate(landmap):
+    print(str(i) + ":", end="")
+    for j,area in enumerate(line):
+        if (i % 2 == 0 or j % 3== 0) and area == "森":
+           print("＋", end="")
+        else:
+            print(area, end="")
+    print()
