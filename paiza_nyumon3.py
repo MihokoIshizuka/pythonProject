ff551fc2741cc97bf67afce2fde27c9cd0714fb2
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-10-01 21:52:42 +0900

    関数

--- paiza_nyumon3.py
+++ paiza_nyumon3.py
@@ -24,3 +24,24 @@ def attack(enemy):
 enemies = ["スライム", "モンスター", "ドラゴン"]
 for enemy in enemies:
     attack(enemy)
+
+
+# 可変長引数
+def introduce(greeting, *names):
+    for name in names:
+        print("私は" + name + "です。" + greeting)
+
+introduce("こんにちは", "勇者", "村人", "兵士")
+
+
+# 可変長引数-辞書
+def introduce(**people):
+    for name, greeting in people.items():
+        print("私は" + name + "です。" + greeting)
+
+introduce(hero="初めまして", villager="こんにちは", solder="よろしくお願いします")
+
+# キーワード引数
+def introduce(name = "私は", role = "村人"):
+    print(name + role + "です")
+introduce()
