f6530686e6fca46a026e1edc93743ed7c18c3a66
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-10-04 22:18:57 +0900

    継承

--- paiza_nyumon3.py
+++ paiza_nyumon3.py
@@ -137,3 +137,27 @@ box.open()
 jewelrybox = JewelryBox("魔法の指輪")
 jewelrybox.look()
 jewelrybox.open()
+
+# メソッドのオーバーライド
+class Box:
+    def __init__(self, item):
+        self.item = item
+
+    def open(self):
+        print("宝箱を開いた。" + self.item + "を手に入れた。")
+
+
+class MagicBox(Box):
+    def look(self):
+        print("宝箱は妖しく輝いている。")
+
+    def open(self):
+        print("宝箱を開いた。" + self.item + "が襲ってきた！")
+
+
+box = Box("鋼鉄の剣")
+box.open()
+
+magicbox = MagicBox("モノマネモンスター")
+magicbox.look()
+magicbox.open()
