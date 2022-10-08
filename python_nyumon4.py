c634fe6b375211f9512f1ca516b18993a87ed65a
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-10-08 23:47:11 +0900

    paiza 問題集

--- python_nyumon4.py
+++ python_nyumon4.py
@@ -3,3 +3,10 @@ a = []
 a = [int(x) for x in input().split()]
 for ans in a:
     print(ans)
+
+# 2 行目で与えられる N 個の整数の入力
+n = int(input())
+N = [n]
+N = [int(x) for x in input().split()]
+for ans in N:
+    print(ans)
