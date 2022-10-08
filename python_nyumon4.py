c70899e31b5030503ed1d41a78e77ef902981d12
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-10-09 00:45:13 +0900

    paiza 問題集

--- python_nyumon4.py
+++ python_nyumon4.py
@@ -10,3 +10,18 @@ N = [n]
 N = [int(x) for x in input().split()]
 for ans in N:
     print(ans)
+
+# バスの運賃
+n, m = (int(x) for x in input().split())
+point = 0
+for i in range(m):
+    price = int(input())
+    if point >= price:
+        point -= price
+        print(n, end =" ")
+        print(point)
+    else:
+        n -= price
+        point += price // 10
+        print(n, end = " ")
+        print(point)
